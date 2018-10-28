0df581af52ad59d90ae9ce6912a181203e0ab68b
19avenkatesh <44552166+19avenkatesh@users.noreply.github.com>
2018-10-28 13:30:24 -0400

    Update Vex2018/Sci-Motor Drive Actual.c

---
+++ Vex2018/Sci-Motor Drive Actual.c
@@ -0,0 +1,21 @@
+#pragma config(Motor,  port2,           frontRightWheelMotor,   tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port3,           frontLeftWheelMotor,    tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port4,           backRightWheelMotor,   tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port5,           backLeftWheelMotor,    tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port6,           rightScissorMotor,  tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port7,           leftScissorMotor, tmotorVex393_MC29, openLoop)
+#define MAX_MOTOR_SPEED 127
+#define MIN_MOTOR_SPEED (-127)
+
+task main()
+{
+	while(true) {
+		motor(frontRightWheelMotor) = vexRT(Ch2);
+		motor(frontLeftWheelMotor) = vexRT(Ch1);
+		motor(backRightWheelMotor) = vexRT(Ch2);
+		motor(backLeftWheelMotor) = vexRT(Ch1);
+
+		motor[rightScissorMotor] = vexRT(Btn5U)*127-vexRT(Btn5D)*127;
+		motor[leftScissorMotor] = vexRT(Btn5U)*127-vexRT(Btn5D)*127;
+	}
+}
