23206c9bbcab3a81649db0db9d49478ddd6c1e39
19avenkatesh <44552166+19avenkatesh@users.noreply.github.com>
2018-11-11 17:01:29 -0500

    Update Vex2018/Sci-Motor Drive Actual.c

--- Vex2018/Sci-Motor Drive Actual.c
+++ Vex2018/Sci-Motor Drive Actual.c
@@ -1,24 +1,39 @@
+<<<<<<< HEAD
 #pragma config(Motor,  port2,           frontRightWheelMotor,   tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port3,           frontLeftWheelMotor,    tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port4,           backRightWheelMotor,   tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port5,           backLeftWheelMotor,    tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port6,           rightScissorMotor,  tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port7,           leftScissorMotor, tmotorVex393_MC29, openLoop)
+#pragma config gyt(Motor,  port7,           leftScissorMotor, tmotorVex393_MC29, openLoop)
 #define MAX_MOTOR_SPEED 127
 #define MIN_MOTOR_SPEED (-127)
+=======
+#pragma config(Motor,  port2,           frontRightWheelMotor, tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port3,           frontLeftWheelMotor,  tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port4,           backRightWheelMotor,  tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port5,           backLeftWheelMotor,   tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port6,           rightScissorMotor,    tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port7,           leftScissorMotor,     tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port8,           rightClaw,            tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port9,           leftClaw,             tmotorVex393_MC29, openLoop)
+
+>>>>>>> 7fef820fea47a8000a6501cbf3997426075ba708
 
 task main()
 {
-	while(true) {
+	while(1==1) {
 		motor(frontRightWheelMotor) = vexRT(Ch2);
 		motor(frontLeftWheelMotor) = vexRT(Ch1);
-		motor(backRightWheelMotor) = vexRT(Ch2);
-		motor(backLeftWheelMotor) = vexRT(Ch1);
 
 		motor[rightScissorMotor] = vexRT(Btn5U)*127+vexRT(Btn5D)*-127;
 		motor[leftScissorMotor] = vexRT(Btn5U)*127+vexRT(Btn5D)*-127;
 	}
 }
+<<<<<<< HEAD
+=======
 
 // adi is not reading this
 //adi thinks there is no change but he is wrong
+//adi has no life
+// ADD CLAW DESIGN DECREASE SPEED
+>>>>>>> 7fef820fea47a8000a6501cbf3997426075ba708
