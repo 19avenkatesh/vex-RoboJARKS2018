0cd38f45eb96a85bfc15621e275a0399a67c4bfc
sahithig <44552170+sahithig@users.noreply.github.com>
2018-11-11 17:21:35 -0500

    Update Vex2018/autaonomos things that probs dont work.c

--- Vex2018/autaonomos things that probs dont work.c
+++ Vex2018/autaonomos things that probs dont work.c
@@ -16,36 +16,36 @@ task main()
 
 
 
-	motor(frontRightWheelMotor) = 127
-	motor(frontLeftWheelMotor) = 127
-	motor(backLeftWheelMotor) = 127
-	motor(backRightWheelMotor) = 127
-	wait1000Msec
+	motor(frontRightWheelMotor) = 127;
+	motor(frontLeftWheelMotor) = 127;
+	motor(backLeftWheelMotor) = 127;
+	motor(backRightWheelMotor) = 127;
+	wait1000Msec;
 
-	motor(frontRightWheelMotor) = 127
-	motor(frontLeftWheelMotor) = -0
-	motor(backLeftWheelMotor) = -0
-	motor(backRightWheelMotor) = 127
-	wait1000Msec
+	motor(frontRightWheelMotor) = 127;
+	motor(frontLeftWheelMotor) = -0;
+	motor(backLeftWheelMotor) = -0;
+	motor(backRightWheelMotor) = 127;
+	wait1000Msec;
 
-	motor(frontRightWheelMotor) = 127
-	motor(frontLeftWheelMotor) = 127
-	motor(backLeftWheelMotor) = 127
-	motor(backRightWheelMotor) = 127
-	wait1000Msec
+	motor(frontRightWheelMotor) = 127;
+	motor(frontLeftWheelMotor) = 127;
+	motor(backLeftWheelMotor) = 127;
+	motor(backRightWheelMotor) = 127;
+	wait1000Msec;
 
 
 	//move forward for 1 second at 127 power
 
 
-	motor(rightClawMotor) = 50
-	motor(leftClawMotor) = 50
-	wait1000Msec
+	motor(rightClawMotor) = 50;
+	motor(leftClawMotor) = 50;
+	wait1000Msec;
 
 	//move claw up I think for 1 second
 
 
-	motor(frontRightWheelMotor) = -127
-	motor(frontLeftWheelMotor) = 127
-	motor(backLeftWheelMotor) = 127
-	motor(backRightWheelMotor) = -127
+	motor(frontRightWheelMotor) = -127;
+	motor(frontLeftWheelMotor) = 127;
+	motor(backLeftWheelMotor) = 127;
+	motor(backRightWheelMotor) = -127;
